c0baab7060babf5a160943ff9eb0dc64e627b73d
anilkumarjnv <anilkumar2000jnv@gmail.com>
2024-12-16 23:09:37 +0530

    Your commit message here

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import StickyScrollRevealDemo from "../StickyScrollReveal/StickyScrollReveal";
 
 const HeroSection = () => {
   const [showHero, setShowHero] = useState(true); // State to toggle between screens
@@ -10,7 +11,7 @@ const HeroSection = () => {
   return (
     <div>
       {showHero ? (
-        <div className="relative bg-[#e6fbee] text-[#334155] h-screen flex flex-col items-center justify-center font-serif animate-fadeIn transition-opacity duration-1000">
+        <div className="relative  bg-[#e6fbee] text-[#334155] h-screen flex flex-col items-center justify-center font-serif animate-fadeIn transition-opacity duration-1000">
           {/* Hero Section */}
           <div className="text-center max-w-3xl px-6">
             <h1 className="text-5xl sm:text-6xl tracking-tight text-[#0B8470]">
@@ -24,19 +25,14 @@ const HeroSection = () => {
                 onClick={handleButtonClick} // Add click handler
                 className="rounded-md bg-[#0b8470] px-5 py-3 text-sm font-semibold text-[#FFFFFF] shadow-lg hover:bg-[#0b8470] transition"
               >
-                Ready to find out how?
+                find out
               </button>
             </div>
           </div>
         </div>
       ) : (
-        <div className="relative bg-[#f5f5f5] text-[#334155] h-screen flex flex-col items-center justify-center font-serif animate-fadeIn transition-all duration-1000">
-          {/* Next Screen */}
-          <h1 className="text-4xl text-[#0B8470]">Welcome to the Next Screen!</h1>
-          <p className="mt-4 text-lg text-[#0B8470]">
-            Here’s what you can do next.
-          </p>
-        </div>
+        <div className=" rounded-md">
+        <StickyScrollRevealDemo/></div>
       )}
     </div>
   );
---
+++ src/components/ui/sticky-scroll-reveal.jsx
@@ -0,0 +1,95 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { useMotionValueEvent, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+export const StickyScroll = ({
+  content,
+  contentClassName
+}) => {
+  const [activeCard, setActiveCard] = React.useState(0);
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    // uncomment line 22 and comment line 23 if you DONT want the overflow container and want to have it change on the entire page scroll
+    // target: ref
+    container: ref,
+    offset: ["start start", "end start"],
+  });
+  const cardLength = content.length;
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    const cardsBreakpoints = content.map((_, index) => index / cardLength);
+    const closestBreakpointIndex = cardsBreakpoints.reduce((acc, breakpoint, index) => 
+      {
+      const distance = Math.abs(latest - breakpoint);
+      if (distance-0.1 < Math.abs(latest - cardsBreakpoints[acc])) {
+        return index;
+      }
+      return acc;
+    }, 0);
+    setActiveCard(closestBreakpointIndex);
+  });
+
+  const backgroundColors = [
+    "#FFFFFF",
+  ];
+  const linearGradients = [
+    "linear-gradient(to bottom right, var(--cyan-500), var(--emerald-500))",
+    "linear-gradient(to bottom right, var(--pink-500), var(--indigo-500))",
+    "linear-gradient(to bottom right, var(--orange-500), var(--yellow-500))",
+  ];
+
+  const [backgroundGradient, setBackgroundGradient] = useState(linearGradients[0]);
+
+  useEffect(() => {
+    setBackgroundGradient(linearGradients[activeCard % linearGradients.length]);
+  }, [activeCard]);
+
+  return (
+    (<motion.div
+      animate={{
+        backgroundColor: backgroundColors[activeCard % backgroundColors.length],
+      }}
+      className="w-full h-screen overflow-y-auto flex justify-center relative  rounded-3xl scrollbar-none  "
+      ref={ref}>
+      <div className="div relative flex items-start px-4 w-full lg:w-3/4">
+        <div className="max-w-full mx-10 ">
+          {content.map((item, index) => (
+            <div key={item.title + index} className="my-40">
+              <motion.h2
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: activeCard === index ? 1 : 0.3,
+                }}
+                className="text-5xl  text-[#0B8470]">
+                {item.title}
+              </motion.h2>
+              <motion.p
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: activeCard === index ? 1 : 0.3,
+                }}
+                className="text-2xl text-[#0B8470] max-w-full mt-10">
+                {item.description}
+              </motion.p>
+            </div>
+          ))}
+          <div className="h-40" />
+        </div>
+      </div>
+      <div
+        style={{ background: backgroundGradient }}
+        className={cn(
+          "hidden lg:block h-96 w-2/4 rounded-3xl  sticky mx-4 top-40 overflow-hidden",
+          contentClassName
+        )}>
+        {content[activeCard].content ?? null}
+      </div>
+    </motion.div>)
+  );
+};
